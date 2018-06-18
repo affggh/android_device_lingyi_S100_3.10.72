b022f00a440b5905a24efd345de92b77434800e3
Ibrahim Fathelbab <akbon19@gmail.com>
2018-06-18 07:02:18 +0200

    Certified FIX #1

--- aicp.mk
+++ aicp.mk
@@ -18,12 +18,13 @@ $(call inherit-product, device/Infinix/x510/device_x510.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := x510
 PRODUCT_NAME := aicp_x510
-PRODUCT_BRAND := Infinix
-PRODUCT_MODEL := Infinix x510
-PRODUCT_MANUFACTURER := Infinix
+PRODUCT_BRAND := infinix
+PRODUCT_MODEL := Infinix_x510
+PRODUCT_MANUFACTURER := INIFNIX
 
-# SuperSU
-PRODUCT_COPY_FILES += \
-	vendor/aicp/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
-	vendor/aicp/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=Infinix/cm_x510/x510:7.1.2/NJH47F/ibrahim_02051933:userdebug/release-keys \
+    PRIVATE_BUILD_DESC="cm_x510-userdebug 7.1.2 NJH47F dev.ibrahim_.20180205.193358 release-keys"
 
+#SuperUser
+WITH_SU := true
